4ab56d0cfe8ffc7212f64f8e78ba0bb286fb471e
Gleb385 <chopper38542@gmail.com>
2024-10-04 17:35:04 +0300

    add phone and adress1

--- client/src/components/OrderForm.js
+++ client/src/components/OrderForm.js
@@ -22,12 +22,14 @@ const OrderForm = () => {
     event.preventDefault();
     console.log("handleSubmit called with name:", name, "email:", email);
     axios
-      .post("/api/order", { name, email, phone, address})
+      .post("/api/order", { name, email, phone, address })
       .then(response => {
         console.log("Axios response received:", response.data);
         setMessage(response.data.message);
         setName(""); // Очистка имени
         setEmail(""); // Очистка email
+        setAddress("");
+        setPhone("");
         console.log("Name and Email reset");
         setTimeout(() => {
           setMessage("");
@@ -38,35 +40,62 @@ const OrderForm = () => {
       });
   };
 
-  return <div>
+  return (
+    <div>
       <h2>Order Tickets</h2>
       <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" value={name} onChange={handleNameChange} autoComplete="name" required />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            autoComplete="name"
+            required
+          />
         </label>
         <br />
         <label>
           Phone:
-          <input type="text" value={phone} onChange={e => setPhone(e.target.value)} required />
+          <input
+            type="text"
+            value={phone}
+            onChange={e => setPhone(e.target.value)}
+            required
+          />
         </label>
         <br />
         <label>
           Address:
-          <input type="text" value={address} onChange={e => setAddress(e.target.value)} required />
+          <input
+            type="text"
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+            required
+          />
         </label>
         <br />
         <label>
           Email:
-          <input type="email" name="email" value={email} onChange={handleEmailChange} autoComplete="email" required />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+            autoComplete="email"
+            required
+          />
         </label>
         <br />
         <button type="submit">Place Order</button>
       </form>
-      {message && <p>
+      {message &&
+        <p>
           {message}
         </p>}
-    </div>;
+    </div>
+  );
 };
 
 export default OrderForm;
