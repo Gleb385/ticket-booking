512cd7d131b1ac0ea9948df67caefa5ab40d2ac5
Gleb385 <chopper38542@gmail.com>
2025-06-02 12:38:40 +0300

    Обновлен App.js, добавлен дизайн Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Импортируем Link
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Container, Button } from "@mui/material";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+
 import OrderForm from "./components/OrderForm";
 import Home from "./components/home";
 import Tickets from "./components/tickets";
@@ -9,10 +12,28 @@ import OrdersList from "./components/OrdersList";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to Ticket Booking</h1>
-        </header>
+      <AppBar position="static">
+        <Toolbar>
+          <FlightTakeoffIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Авиабилеты онлайн
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Главная
+          </Button>
+          <Button color="inherit" component={Link} to="/tickets">
+            Билеты
+          </Button>
+          <Button color="inherit" component={Link} to="/order">
+            Заказ
+          </Button>
+          <Button color="inherit" component={Link} to="/orders">
+            Мои заказы
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="md" sx={{ mt: 4 }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tickets" element={<Tickets />} />
@@ -20,7 +41,7 @@ function App() {
           <Route path="/order/:id" element={<Order />} />
           <Route path="/orders" element={<OrdersList />} />
         </Routes>
-      </div>
+      </Container>
     </Router>
   );
 }
